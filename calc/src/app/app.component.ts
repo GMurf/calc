ddf287c2758c63cb64fbb8f17a3af1dd0436fe37
george murph <murfg25@gmail.com>
2020-08-17 17:56:03 -0400

    Added additon operation.

--- calc/src/app/app.component.ts
+++ calc/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'calc';
-}
+
+
+
+export class AppComponent implements OnInit {
+  title:string = 'calc';
+  total:number = 0;
+  calculation:string ='';
+  message:string;
+  magnitud:number = 10;
+  stack:(number)[] = [];
+  clear(){this.calculation='0'; this.total=0; this.message ="cleared";this.stack =[];};
+  add_number(value:string):void{
+    
+    this.calculation = (parseInt(value) + ( parseInt(this.calculation) * this.magnitud)).toString();
+    
+    this.message =  this.magnitud.toString();
+  };
+  add(){this.stack.push(parseInt(this.calculation));console.log(this.stack); this.calculation = '0'; this.message = this.stack.toString();};
+  solve():void{
+    this.stack.push(parseInt(this.calculation));
+    let result:number = 0;
+    const reducer = (accumulator, currentvalue) => accumulator + currentvalue;
+    
+
+    this.message = this.stack.reduce(reducer).toString();
+    this.total = this.stack.reduce(reducer);
+    
+  }
+
+  
+  ngOnInit() {
+    this.calculation='0';
+  
+}}
+
+class Operations {
+  negative(value:number):number{
+    return value * -1;
+  }
+
+  sum(a:number, b:number):number{
+    return a + b;
+  }
+  subtract(a:number, b:number):number{
+    return a - b;
+  }
+
+
+  }
--- calc/src/app/app.module.ts
+++ calc/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatButtonModule, MatCardModule } from '@angular/material';
+
+import { FormsModule } from "@angular/forms";
+
+
+
+const matmodules = [
+  MatTableModule,
+  MatCardModule,
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    matmodules,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
