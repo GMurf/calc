9a0dd71827be608c840f25e6c566c5a4895105ad
george murph <murfg25@gmail.com>
2020-08-19 23:29:45 -0400

    angular

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/hardwarestore'));
+app.use(express.static(__dirname + '/dist/HardwareStore'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/hardwarestore/index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/HardwareStore/index.html'));});
     app.listen(process.env.PORT || 8080);
